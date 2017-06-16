0db5b28997b96defc73c8aa1ae8fe84ca1cbbe60
Michael Jackson <majgis@gmail.com>
2017-06-15 23:38:31 -0600

    Extract tokens from template and write to console

--- main.go
+++ main.go
@@ -23,5 +23,45 @@ func main() {
 		fmt.Println("The template file could not be read: " + templatePath)
 		os.Exit(1)
 	}
-	fmt.Println(string(contents))
+	template, err := marshallTemplate(contents)
+	if err != nil {
+		fmt.Println("The template is malformed: " + err.Error())
+		os.Exit(1)
+	}
+	for _, v := range template.tokens {
+		fmt.Println(string(v))
+	}
+}
+
+type htmlTemplate struct {
+	sections [][]byte
+	tokens   [][]byte
+}
+
+// Marshall template into htmlTemplate
+func marshallTemplate(template []byte) (htmlTemplate, error) {
+	t := htmlTemplate{}
+	startCount := 0
+	startPosition := 0
+	endCount := 0
+	endPosition := 0
+	var tokens [][]byte
+	for i, v := range template {
+		if v == '{' {
+			startCount++
+			if startCount == 2 {
+				startCount = 0
+				startPosition = i + 1
+			}
+		} else if v == '}' {
+			endCount++
+			if endCount == 2 {
+				endCount = 0
+				endPosition = i - 2
+				tokens = append(tokens, template[startPosition:endPosition+1])
+			}
+		}
+	}
+	t.tokens = tokens
+	return t, nil
 }
