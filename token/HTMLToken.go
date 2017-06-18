3c8e83fba5975222bd0652dbfc9ef4797fca7fae
Michael Jackson <majgis@gmail.com>
2017-06-18 15:51:07 -0600

    Add missing comment to HTMLToken.go

--- token/HTMLToken.go
+++ token/HTMLToken.go
@@ -3,7 +3,7 @@ package token
 // HTMLToken is an in-memory object for the token within an HTML Template
 // URI: The URI to the endpoint that will provide HTML snippet
 // CacheMS: The duration in milliseconds that the HTML snippet should be stored
-// TimeoutMS:
+// TimeoutMS: The duration in milliseconds to wait before timing out
 type HTMLToken struct {
 	URI       string
 	CacheMS   int
