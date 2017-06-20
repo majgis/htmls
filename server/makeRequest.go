4a2431a33dd456ac132f5065378a7fbbd6979d34
Michael Jackson <majgis@gmail.com>
2017-06-19 20:20:47 -0600

    Instead of reading entire response body, stream the data

--- server/makeRequest.go
+++ server/makeRequest.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"io/ioutil"
 	"net/http"
 )
 
@@ -11,16 +10,25 @@ import (
 // ch: The channel to write response chunks back to
 func makeRequest(url string, timeoutMS int, ch chan<- responseChunk) {
 	response, err := http.Get(url)
-	responseChunk := responseChunk{}
+
 	// If an error occurred, write error to channel, close channel and exit
 	if err != nil {
-		responseChunk.Error = err
-		ch <- responseChunk
+		ch <- responseChunk{Error: err}
 		close(ch)
 		return
 	}
-	responseChunk.Bytes, responseChunk.Error = ioutil.ReadAll(response.Body)
-	ch <- responseChunk
+
+	// Iterate through chunks of the response body
+	for {
+		chunk := responseChunk{Bytes: make([]byte, 2000, 2000)}
+		byteCount, err := response.Body.Read(chunk.Bytes)
+		ch <- chunk
+		if byteCount == 0 || chunk.Error != nil {
+			chunk.Error = err
+			break
+		}
+	}
 	close(ch)
+
 	return
 }
